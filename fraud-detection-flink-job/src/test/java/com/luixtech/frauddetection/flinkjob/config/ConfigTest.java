4de5457d0ae4e07c55aed2616b30358e619f1bc0
pm6422 <pm6422@126.com>
2023-07-26 19:52:36 +0800

    refactor

--- fraud-detection-flink-job/src/test/java/com/luixtech/frauddetection/flinkjob/config/ConfigTest.java
+++ fraud-detection-flink-job/src/test/java/com/luixtech/frauddetection/flinkjob/config/ConfigTest.java
@@ -1,21 +1,3 @@
-/*
- * Licensed to the Apache Software Foundation (ASF) under one
- * or more contributor license agreements.  See the NOTICE file
- * distributed with this work for additional information
- * regarding copyright ownership.  The ASF licenses this file
- * to you under the Apache License, Version 2.0 (the
- * "License"); you may not use this file except in compliance
- * with the License.  You may obtain a copy of the License at
- *
- *     http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
- */
-
 package com.luixtech.frauddetection.flinkjob.config;
 
 import static org.junit.Assert.assertEquals;
@@ -33,7 +15,7 @@ public class ConfigTest {
     InputConfig inputConfig = InputConfig.fromParameters(parameters);
 
     final String kafkaHost = inputConfig.get(Parameters.KAFKA_HOST);
-    assertEquals("Wrong config parameter retrived", "host-from-args", kafkaHost);
+    assertEquals("Wrong config parameter retrieved", "host-from-args", kafkaHost);
   }
 
   @Test
@@ -43,6 +25,6 @@ public class ConfigTest {
     InputConfig inputConfig = InputConfig.fromParameters(parameters);
 
     final Integer kafkaPort = inputConfig.get(Parameters.KAFKA_PORT);
-    assertEquals("Wrong config parameter retrived", new Integer(9092), kafkaPort);
+    assertEquals("Wrong config parameter retrieved", Integer.valueOf(9092), kafkaPort);
   }
 }
