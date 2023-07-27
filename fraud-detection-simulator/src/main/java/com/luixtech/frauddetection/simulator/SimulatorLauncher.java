0778951110485d346be8a0cd26033d1544bfcfcd
louis <louis@luixtech.com>
2023-07-27 09:45:01 +0800

    refactor

--- fraud-detection-simulator/src/main/java/com/luixtech/frauddetection/simulator/SimulatorLauncher.java
+++ fraud-detection-simulator/src/main/java/com/luixtech/frauddetection/simulator/SimulatorLauncher.java
@@ -3,18 +3,17 @@ package com.luixtech.frauddetection.simulator;
 import com.luixtech.framework.EnableLuixFramework;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.ComponentScan;
+
 @SpringBootApplication
 @EnableLuixFramework
-@ComponentScan("com.luixtech.frauddetection.simulator")
 public class SimulatorLauncher {
-  /**
-   * Entrance method which used to run the application. Spring profiles can be configured with a program arguments
-   * --spring.profiles.active=your-active-profile
-   *
-   * @param args program arguments
-   */
-  public static void main(String[] args) {
-    SpringApplication.run(SimulatorLauncher.class, args);
-  }
+    /**
+     * Entrance method which used to run the application. Spring profiles can be configured with a program arguments
+     * --spring.profiles.active=your-active-profile
+     *
+     * @param args program arguments
+     */
+    public static void main(String[] args) {
+        SpringApplication.run(SimulatorLauncher.class, args);
+    }
 }
