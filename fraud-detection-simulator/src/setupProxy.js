41a2b5774a503d541b6aa8966751e6e974869810
louis <louis@luixtech.com>
2023-07-27 14:53:39 +0800

    delete react

--- fraud-detection-simulator/src/setupProxy.js
+++
@@ -1,6 +0,0 @@
-const proxy = require("http-proxy-middleware");
-
-module.exports = function(app) {
-  app.use(proxy("/api", { target: "http://localhost:5656" }));
-  app.use(proxy("/ws", { target: "ws://localhost:5656", ws: true }));
-};
