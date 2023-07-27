6412b73b67646dffeacbc48f838c87477976c74d
louis <louis@luixtech.com>
2023-07-27 15:45:57 +0800

    refactor

--- fraud-detection-flink-job/src/main/java/com/luixtech/frauddetection/flinkjob/input/Parameters.java
+++ fraud-detection-flink-job/src/main/java/com/luixtech/frauddetection/flinkjob/input/Parameters.java
@@ -11,26 +11,17 @@ public class Parameters {
 
     private static final Map<InputParam<?>, Object> HOLDER = new HashMap<>();
 
-    public <T> Parameters(
-            ParameterDefinitions parameterDefinitions,
-            List<InputParam<String>> stringInputParams,
-            List<InputParam<Integer>> intInputParams,
-            List<InputParam<Boolean>> boolInputParams) {
-        overrideDefaults(stringInputParams, parameterDefinitions);
-        overrideDefaults(intInputParams, parameterDefinitions);
-        overrideDefaults(boolInputParams, parameterDefinitions);
-
+    <T> Parameters(ParameterDefinitions parameterDefinitions, List<InputParam<?>> all) {
+        overrideDefaults(all, parameterDefinitions);
         HOLDER.forEach((key, value) -> log.info("{} = {}", key.getName(), value));
-        System.out.println();
     }
 
     public static Parameters fromDefinitions(ParameterDefinitions parameterDefinitions) {
-        return new Parameters(
-                parameterDefinitions, ParameterDefinitions.STRING_INPUT_PARAMS, ParameterDefinitions.INT_INPUT_PARAMS, ParameterDefinitions.BOOL_INPUT_PARAMS);
+        return new Parameters(parameterDefinitions, ParameterDefinitions.ALL);
     }
 
-    private <T> void overrideDefaults(List<InputParam<T>> inputParams, ParameterDefinitions parameterDefinitions) {
-        for (InputParam<T> inputParam : inputParams) {
+    private <T> void overrideDefaults(List<InputParam<?>> inputParams, ParameterDefinitions parameterDefinitions) {
+        for (InputParam<?> inputParam : inputParams) {
             HOLDER.put(inputParam, parameterDefinitions.getOrDefaultValue(inputParam));
         }
     }
