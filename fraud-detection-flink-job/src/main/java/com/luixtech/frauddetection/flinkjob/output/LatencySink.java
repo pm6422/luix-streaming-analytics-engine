8333b5ba1186c02c35afa3a30d09f3632fd2fb05
pm6422 <pm6422@126.com>
2023-07-29 22:10:26 +0800

    import spi

--- fraud-detection-flink-job/src/main/java/com/luixtech/frauddetection/flinkjob/output/LatencySink.java
+++ fraud-detection-flink-job/src/main/java/com/luixtech/frauddetection/flinkjob/output/LatencySink.java
@@ -1,43 +1,16 @@
 package com.luixtech.frauddetection.flinkjob.output;
 
 import com.luixtech.frauddetection.flinkjob.input.Arguments;
-import com.luixtech.frauddetection.flinkjob.utils.KafkaUtils;
-import org.apache.flink.api.common.serialization.SimpleStringSchema;
-import org.apache.flink.connector.base.DeliveryGuarantee;
-import org.apache.flink.connector.kafka.sink.KafkaRecordSerializationSchema;
-import org.apache.flink.connector.kafka.sink.KafkaSink;
+import com.luixtech.frauddetection.flinkjob.output.sinkcreator.SinkCreator;
 import org.apache.flink.streaming.api.datastream.DataStream;
 import org.apache.flink.streaming.api.datastream.DataStreamSink;
-import org.apache.flink.streaming.api.functions.sink.PrintSinkFunction;
-
-import java.util.Properties;
 
 public class LatencySink {
 
     public static DataStreamSink<String> addLatencySink(Arguments arguments, DataStream<String> stream) {
-        DataStreamSink<String> dataStreamSink;
-
-        switch (arguments.messageChannel) {
-            case "kafka":
-                Properties kafkaProps = KafkaUtils.initProducerProperties(arguments);
-                KafkaSink<String> kafkaSink =
-                        KafkaSink.<String>builder()
-                                .setKafkaProducerConfig(kafkaProps)
-                                .setRecordSerializer(
-                                        KafkaRecordSerializationSchema.builder()
-                                                .setTopic(arguments.latencyTopic)
-                                                .setValueSerializationSchema(new SimpleStringSchema())
-                                                .build())
-                                .setDeliverGuarantee(DeliveryGuarantee.NONE)
-                                .build();
-                dataStreamSink = stream.sinkTo(kafkaSink);
-                break;
-            case "socket":
-                dataStreamSink = stream.addSink(new PrintSinkFunction<>(true));
-                break;
-            default:
-                throw new IllegalArgumentException("Invalid latency sink " + arguments.messageChannel);
-        }
+        DataStreamSink<String> dataStreamSink = SinkCreator
+                .getInstance("latency-" + arguments.messageChannel)
+                .create(stream, arguments);
         return dataStreamSink;
     }
 }
