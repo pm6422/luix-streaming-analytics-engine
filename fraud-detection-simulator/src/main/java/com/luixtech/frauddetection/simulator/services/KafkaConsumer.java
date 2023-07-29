7069781ee9a3382cf50d2bab9000f6e45bb2901b
pm6422 <pm6422@126.com>
2023-07-29 15:57:24 +0800

    delete current-rule

--- fraud-detection-simulator/src/main/java/com/luixtech/frauddetection/simulator/services/KafkaConsumer.java
+++ fraud-detection-simulator/src/main/java/com/luixtech/frauddetection/simulator/services/KafkaConsumer.java
@@ -1,9 +1,6 @@
 package com.luixtech.frauddetection.simulator.services;
 
-import com.fasterxml.jackson.databind.ObjectMapper;
-import com.luixtech.frauddetection.common.dto.Rule;
 import com.luixtech.frauddetection.simulator.config.ApplicationProperties;
-import com.luixtech.frauddetection.simulator.domain.RulePayload;
 import com.luixtech.frauddetection.simulator.repository.RuleRepository;
 import lombok.AllArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
@@ -12,15 +9,11 @@ import org.springframework.messaging.handler.annotation.Payload;
 import org.springframework.messaging.simp.SimpMessagingTemplate;
 import org.springframework.stereotype.Service;
 
-import java.io.IOException;
-import java.util.Optional;
-
 @Service
 @AllArgsConstructor
 @Slf4j
 public class KafkaConsumer {
 
-    private static final ObjectMapper          OBJECT_MAPPER = new ObjectMapper();
     private final        SimpMessagingTemplate simpTemplate;
     private final        RuleRepository        ruleRepository;
     private final        ApplicationProperties applicationProperties;
