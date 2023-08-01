43e027f37754715f41fedf580a84edf1c9349d21
louis <louis@luixtech.com>
2023-08-01 08:50:52 +0800

    refactor

--- fraud-detection-flink-job/src/test/java/com/luixtech/frauddetection/flinkjob/core/util/BroadcastStreamNonKeyedOperatorTestHarness.java
+++ fraud-detection-flink-job/src/test/java/com/luixtech/frauddetection/flinkjob/core/util/BroadcastStreamNonKeyedOperatorTestHarness.java
@@ -1,21 +1,3 @@
-/*
- * Licensed to the Apache Software Foundation (ASF) under one
- * or more contributor license agreements.  See the NOTICE file
- * distributed with this work for additional information
- * regarding copyright ownership.  The ASF licenses this file
- * to you under the Apache License, Version 2.0 (the
- * "License"); you may not use this file except in compliance
- * with the License.  You may obtain a copy of the License at
- *
- *     http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
- */
-
 package com.luixtech.frauddetection.flinkjob.core.util;
 
 import java.util.Arrays;
@@ -36,8 +18,7 @@ import org.apache.flink.util.Preconditions;
  * and watermarks into the operator. {@link java.util.Deque}s containing the emitted elements and
  * watermarks can be retrieved. you are free to modify these.
  */
-public class BroadcastStreamNonKeyedOperatorTestHarness<IN1, IN2, OUT>
-    extends AbstractStreamOperatorTestHarness<OUT> {
+public class BroadcastStreamNonKeyedOperatorTestHarness<IN1, IN2, OUT> extends AbstractStreamOperatorTestHarness<OUT> {
 
   private final CoBroadcastWithNonKeyedOperator<IN1, IN2, OUT> twoInputOperator;
 
