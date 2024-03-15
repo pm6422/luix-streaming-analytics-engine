c203f9ca4942bcdf2f45c73de347e5caa80b9bd9
louis <pm6422@126.com>
2024-03-15 16:33:24 +0800

    re

--- luix-fraud-detection-simulator/src/main/java/com/luixtech/frauddetection/simulator/controllers/SyncRuleController.java
+++ luix-fraud-detection-simulator/src/main/java/com/luixtech/frauddetection/simulator/controllers/SyncRuleController.java
@@ -13,22 +13,21 @@ import org.springframework.web.bind.annotation.RestController;
 import java.util.List;
 
 @RestController
-@RequestMapping("/api")
 @AllArgsConstructor
-public class FlinkController {
+public class SyncRuleController {
 
     private final DetectorRuleRepository detectorRuleRepository;
     private final KafkaRuleProducer      kafkaRuleProducer;
 
-    @GetMapping("/flink/add-all-rules")
-    public void addAllRules() {
+    @GetMapping("/api/rules/sync-all")
+    public void syncAllRules() {
         List<DetectorRule> detectorRules = detectorRuleRepository.findAll();
         for (DetectorRule detectorRule : detectorRules) {
             kafkaRuleProducer.addRule(detectorRule.toRuleCommand());
         }
     }
 
-    @GetMapping("/flink/delete-all-rules")
+    @GetMapping("/api/rules/delete-all")
     public void deleteAllRules() {
         RuleCommand ruleCommand = new RuleCommand();
         ruleCommand.setControl(Control.DELETE_ALL);
