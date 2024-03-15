527ff2d132684724da60f0b278c9f1a8f9ebe5cf
louis <pm6422@126.com>
2024-03-15 17:22:19 +0800

    re

--- luix-fraud-detection-simulator/src/main/java/com/luixtech/frauddetection/simulator/controllers/SyncRuleController.java
+++ luix-fraud-detection-simulator/src/main/java/com/luixtech/frauddetection/simulator/controllers/SyncRuleController.java
@@ -7,7 +7,6 @@ import com.luixtech.frauddetection.simulator.kafka.producer.KafkaRuleProducer;
 import com.luixtech.frauddetection.simulator.repository.DetectorRuleRepository;
 import lombok.AllArgsConstructor;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
