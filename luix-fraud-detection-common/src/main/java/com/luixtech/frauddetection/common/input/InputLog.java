e6cf12fc4c1cdee7f8549e83ca2388ae4ae638bd
louis <pm6422@126.com>
2024-03-18 17:25:03 +0800

    refactor

--- luix-fraud-detection-common/src/main/java/com/luixtech/frauddetection/common/input/InputLog.java
+++ luix-fraud-detection-common/src/main/java/com/luixtech/frauddetection/common/input/InputLog.java
@@ -1,16 +1,28 @@
 package com.luixtech.frauddetection.common.input;
 
+import com.luixtech.frauddetection.common.IngestionTimeAssignable;
 import lombok.Data;
 
 import java.util.Map;
 
 @Data
-public class InputLog {
+public class InputLog implements IngestionTimeAssignable {
 
-    private String recordId;
-
+    private String              recordId;
     private Map<String, Object> record;
-
     private Map<String, Object> recordState;
+    /**
+     * Timestamp of ingestion into the flink input source, unit: ms
+     */
+    private Long                ingestionTime;
 
+    @Override
+    public void setIngestionTime(Long timestamp) {
+        this.ingestionTime = timestamp;
+    }
+
+    @Override
+    public Long getIngestionTime() {
+        return this.ingestionTime;
+    }
 }
