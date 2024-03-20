ce279add549231ef5eedcb24598dd224a4322a51
louis <pm6422@126.com>
2024-03-20 12:17:48 +0800

    refactor

--- luix-content-analytic-engine-flink-job/src/main/java/cn/luixtech/cae/flinkjob/core/Descriptors.java
+++ luix-content-analytic-engine-flink-job/src/main/java/cn/luixtech/cae/flinkjob/core/Descriptors.java
@@ -8,8 +8,8 @@ import org.apache.flink.util.OutputTag;
 
 public class Descriptors {
     public static final MapStateDescriptor<String, RuleCommand> RULES_COMMAND_DESCRIPTOR   = new MapStateDescriptor<>("rules", BasicTypeInfo.STRING_TYPE_INFO, TypeInformation.of(RuleCommand.class));
-    public static final OutputTag<String>                       RULE_EVALUATION_RESULT_TAG = new OutputTag<String>("rule-evaluation-result-sink") {
+    public static final OutputTag<String>                       RULE_EVALUATION_RESULT_TAG = new OutputTag<>("rule-evaluation-result-sink") {
     };
-    public static final OutputTag<Long>                         HANDLING_LATENCY_SINK_TAG  = new OutputTag<Long>("handling-latency-sink") {
+    public static final OutputTag<Long>                         HANDLING_LATENCY_SINK_TAG  = new OutputTag<>("handling-latency-sink") {
     };
 }
