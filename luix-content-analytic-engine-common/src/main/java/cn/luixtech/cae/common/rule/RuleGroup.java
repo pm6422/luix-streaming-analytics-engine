9224e43f498bf956ede6b21aad585adf71bcc857
louis <pm6422@126.com>
2024-03-20 12:01:32 +0800

    refactor

--- luix-content-analytic-engine-common/src/main/java/cn/luixtech/cae/common/rule/RuleGroup.java
+++ luix-content-analytic-engine-common/src/main/java/cn/luixtech/cae/common/rule/RuleGroup.java
@@ -25,6 +25,10 @@ public class RuleGroup {
      */
     private Integer         windowMinutes;
     /**
+     * The rule group will not issue same output for same input record during the silent period to prevent excessive output from interfering. 0 represents no silent period
+     */
+    private Integer         silentMinutes;
+    /**
      * Clear local existing inputs cache after rule matched
      */
     private boolean         resetAfterMatch;
